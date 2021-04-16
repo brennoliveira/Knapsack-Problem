e2becb1a819abf547583a945f7fd9d0ca573f84a
Francisco José <francisco.jsantos@souunit.com.br>
2021-04-16 14:33:08 -0300

    small adjustments at the commentation in code

--- Knapsack.py
+++ Knapsack.py
@@ -2,13 +2,11 @@
 import random
 from deap import algorithms, base, creator, tools
 
-# defining MAX weight
+# Defining MAX weight
 MAX_WEIGHT = 50
 print(f"Max weight is: {MAX_WEIGHT}\n")
-#numberOfItems = 5
 
-
-# items generation function
+# Items generation function
 def generate_items(numberOfItems):
     items = []
     '''Ganerating random items (Weight & Value)
@@ -18,15 +16,17 @@ def generate_items(numberOfItems):
 
     return items
 
-
 items = generate_items(20)
+
 # Showing the generated items
 for item in items:
     print(item)
 
+print("")
 
 # Defining the Fitness
 creator.create("Fitness", base.Fitness, weights=(1.0,))
+
 # Defining individual according to the given Fitness
 creator.create("Individual", list, fitness=creator.Fitness)
 
@@ -50,52 +50,64 @@ def evalOneMax(individual):
         if individual[index] > 0.5:
             value += items[index]['Value']
             weight += items[index]['Weight']
+
     # Checking if the bag isn't overweighted
     if weight > MAX_WEIGHT:
         return 100000000, 0
     return weight, value
 
+# Get items function
 def getItems(individual):
     _items = []
     for index in range(len(individual)):
         if individual[index] > 0.5:
             _items.append((index, items[index]))
     return _items
-
-
+ 
+# Registering the Evaluation method, the Mutation and Selection into the toolbox
 toolbox.register("evaluate", evalOneMax)
 toolbox.register("mate", tools.cxTwoPoint)
 toolbox.register("mutate", tools.mutFlipBit, indpb=0.5)
-
 toolbox.register("select", tools.selNSGA2)
 
+# Defining the population size
 population = toolbox.population(n=300)
 
+# Defining number of generations
 numberOfGen = 50
+
+# Evolution process
 for gen in range(numberOfGen):
 
+    # The algorithms module implements many evolutionary algorithms
+    # https://deap.readthedocs.io/en/master/api/algo.html
+    # The function "varAnd" applies Mutation and Crossover operations
+    # cxpb: Crossover probability
+    # mutpb: Mutation probability
     offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
 
-    # avalia cada indivíduo
+    # Evaluating each individual
     fits = toolbox.map(toolbox.evaluate, offspring)
 
-    # associa cada indivíduo ao seu valor de fitness
+    # Associating each individual to its Fitness value
     for fit, ind in zip(fits, offspring):
         ind.fitness.values = [fit[1]]
 
+    # Applies the selection to generate a new population
     population = toolbox.select(offspring, k=len(population))
 
- #   best = tools.selBest(population, k=1)
+# Returns the best K individuals from the last population
+topX = tools.selBest(population, k=10)
 
-top10 = tools.selBest(population, k=10)
+# Prints the best individual
+print(topX[0])
 
-# Imprime o melhor
-print(top10[0])
-
-for item in getItems(top10[0]):
+# Printing every item
+for item in getItems(topX[0]):
     print(item)
 
 print("")
 
-print(f"{sum(list(map(lambda x: x[1]['Value'], getItems(top10[0]))))}")
-print(f"{sum(list(map(lambda x: x[1]['Weight'], getItems(top10[0]))))}")
+# Printing the total Value and Weight of the bag
+print(f"Total Value: {sum(list(map(lambda x: x[1]['Value'], getItems(topX[0])))):.2f}")
+print(f"Total Weight: {sum(list(map(lambda x: x[1]['Weight'], getItems(topX[0])))):.2f}")
